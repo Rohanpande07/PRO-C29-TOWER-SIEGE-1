bc44c6a333ceef70bba27d9469403803183933c3
Rohanpande07 <78965170+Rohanpande07@users.noreply.github.com>
2021-04-27 17:12:42 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,17 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+    }
+}
